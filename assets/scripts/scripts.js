77bccfeecb48735eeeca98ea26816ee0fb7b7156
Mahdi <mohammad.mz9281@gmail.com>
2022-12-12 18:23:56 +0330

    Add about section. Navbar is not fixed anymore!

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -4,9 +4,9 @@ window.onscroll = function () { toggleGlass() };
 window.onresize = function () { toggleGlass() };
 
 function toggleGlass() {
-    if ((document.documentElement.scrollTop > 1) || (window.innerWidth <= 575)) {
+    if ((window.innerWidth <= 575)) {
         document.getElementById('NAV').classList.add('glass');
-    } else if ((document.documentElement.scrollTop <= 1) && (window.innerWidth > 575)) {
+    } else if (window.innerWidth > 575) {
         document.getElementById('NAV').classList.remove('glass');
     };
 }
