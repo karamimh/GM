80ce611833b39579d14c6821080aec05be905575
Mahdi <mohammad.mz9281@gmail.com>
2022-12-12 18:43:32 +0330

    Prettify the code!!!

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,12 +1,15 @@
-
 toggleGlass();
-window.onscroll = function () { toggleGlass() };
-window.onresize = function () { toggleGlass() };
+window.onscroll = function () {
+  toggleGlass();
+};
+window.onresize = function () {
+  toggleGlass();
+};
 
 function toggleGlass() {
-    if ((window.innerWidth <= 575)) {
-        document.getElementById('NAV').classList.add('glass');
-    } else if (window.innerWidth > 575) {
-        document.getElementById('NAV').classList.remove('glass');
-    };
+  if (window.innerWidth <= 575) {
+    document.getElementById("NAV").classList.add("glass");
+  } else if (window.innerWidth > 575) {
+    document.getElementById("NAV").classList.remove("glass");
+  }
 }
